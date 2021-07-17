a54b9279058658ff43c4047a34a9682a16784503
Nick <nick_r_moore@hotmail.com>
2021-07-17 12:47:57 -0400

    Hello World

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
-public class Main {
+import com.company.lvl3.HelloWorld;
 
+public class Main {
     public static void main(String[] args) {
 	// write your code here
+        HelloWorld.SayHello();
     }
 }
---
+++ src/com/company/lvl3/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.company.lvl3;
+
+public class HelloWorld {
+    public static void SayHello(){
+        System.out.println("Hello World!");
+    }
+}
